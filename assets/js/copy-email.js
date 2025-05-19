15857691e0d6b2765e7def320ff0c6f8e7845b3e
sara rasoulian <sara.rasoulian.dev@gmail.com>
2025-05-19 22:26:34 +0330

    Add copy email

---
+++ assets/js/copy-email.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const emailButton = document.getElementById("email-to-copy");
+  const emailText = emailButton.querySelector(".social-title");
+  const originalText = emailText.textContent;
+
+  emailButton.addEventListener("click", function () {
+    const email = emailButton.getAttribute("data-email");
+    navigator.clipboard.writeText(email);
+
+    emailText.textContent = "Email Copied!";
+
+    setTimeout(() => {
+      emailText.textContent = originalText;
+    }, 1500);
+  });
+});
